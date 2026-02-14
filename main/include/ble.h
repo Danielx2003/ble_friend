4b1aa7e82c1272fce4704dbaa632e08f4d21ea8e
Danielx2003 <ahipsterwalrus4@gnail.com>
2026-02-14 00:40:12 +0000

    feat(ble): created ble wrapper .h file

---
+++ main/include/ble.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct {
+	int reason;
+} ble_event_reset_t;
+
+typedef struct {
+  void (*on_ready)(void);
+  void (*on_reset)(ble_event_reset_t* reset);
+} ble_callbacks_t;
+
+int ble_init(const ble_callbacks_t* cbs);
+int ble_start(void);
+void device_init(void);
