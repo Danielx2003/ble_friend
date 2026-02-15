d57c302476f3a26b8cd396558d3816b68998c586
Dan <70431670+Danielx2003@users.noreply.github.com>
2026-02-15 16:33:59 +0000

    feat(parser): impl BLE specific msg callbacks (#3)
    * feat(parser): added new out_result param for interfacing handlers
    
    * refactor(parser/disc): reworked handlers to be implementation specific

--- main/src/disc_nimble.c
+++ main/src/disc_nimble.c
@@ -9,11 +9,92 @@
 
 static const char* tag = "BLE_DISC";
 
+void ble_handle_lost(void* context, mfg_data_t* mfg);
+void ble_handle_pairing(void* contexnt, mfg_data_t* mfg);
+void ble_handle_paired(void* context, mfg_data_t* mfg);
+int disc_cb(struct ble_gap_event *event, void *arg);
+
+static parser_action_table_t ble_actions = {
+    .on_pairing = ble_handle_pairing,
+    .on_paired  = ble_handle_paired,
+    .on_lost    = ble_handle_lost
+};
+
+static uint8_t own_addr_type;
+
+void ble_handle_lost(void* context, mfg_data_t* mfg)
+{
+  printf("\nHandle Lost Msg\n");
+	struct ble_gap_event* event = (struct ble_gap_event*)context;
+	printf("Event Type: %d\n", event->type);
+}
+
+void ble_handle_pairing(void* context, mfg_data_t* mfg)
+{
+	int rc;
+	
+	printf("\n\nHanlde Pairing Msg\n\n");
+	struct ble_gap_event* event = (struct ble_gap_event*)context;
+	
+	rc = ble_gap_disc_cancel();
+	if (rc != 0) { printf("Failed to cancel\n"); return; }
+	printf("Cancelled! rc=%d\n", rc);
+
+	rc = ble_hs_id_infer_auto(0, &own_addr_type);
+	if (rc != 0) { return; }
+
+ rc = ble_gap_connect(
+		own_addr_type, 
+		&(event->ext_disc.addr),
+		30000,
+		NULL,
+		disc_cb, NULL);
+		
+	printf("Tried to connect to device: Rc=%d\n", rc);
+}
+
+void ble_handle_paired(void* context, mfg_data_t* mfg)
+{
+	struct ble_gap_event* event = (struct ble_gap_event*)context;
+	printf("Event Type: %d\n", event->type);
+}
+
+int connect_cb(struct ble_gap_event *event, void *arg)
+{
+	printf("Type: %d\n", event->type);
+	return 0;
+}
+
 int disc_cb(struct ble_gap_event *event, void *arg)
 {
 	parser_status_t status;
-	status = parse_adv_data(event->ext_disc.data, event->ext_disc.length_data);
+	mfg_data_t mfg;
+	memset(&mfg, 0, sizeof(mfg));
+	parser_result_t result;
+	
+	result.mfg = &mfg;
+	
+	switch(event->type)
+	{
+		case BLE_GAP_EVENT_DISC_COMPLETE:
+			printf("Discovery Complete?\n");			
+			break;
+		case BLE_GAP_EVENT_DISCONNECT:
+			printf("Disconnected\n");
+			break;
+		case BLE_GAP_EVENT_CONNECT:
+			printf("Connected!\n");
+			printf("Connection Handle: %d Status: %d\n", event->connect.conn_handle, event->connect.status);
+			break;
+		default:
+			break;
+	}
+	
+	status = parse_adv_data(event->ext_disc.data, event->ext_disc.length_data, &result);
 	if (status != PARSER_SUCCESS) { return status; }
+
+	result.action(event, result.mfg);
+	
 	return 0;
 }
 
@@ -23,7 +104,8 @@ ble_status_t disc_start(
 )
 {
   int rc;
-  uint8_t own_addr_type;
+	
+	parser_init(&ble_actions);
   
   rc = ble_hs_util_ensure_addr(0);
   if (rc != 0) { ESP_LOGE(tag, "No valid Address available. rc=%d\n", rc); return rc; }
--- main/src/parser.c
+++ main/src/parser.c
@@ -6,21 +6,22 @@
 #include <string.h>
 #include <stddef.h>
 
-parser_status_t parse_adv_data(const uint8_t* adv_data, size_t adv_data_len)
+static parser_action_table_t *g_actions;
+
+parser_status_t parse_adv_data(const uint8_t* adv_data, size_t adv_data_len, parser_result_t* out_result)
 {
   parser_status_t status;
-  mfg_data_t mfg = {0};
 
-  status = extract_mfg_data(adv_data, adv_data_len, &mfg);
+  status = extract_mfg_data(adv_data, adv_data_len, out_result->mfg);
   if (status != PARSER_SUCCESS) { return status; }
   
-  return parse_mfg_data(&mfg);
+  return parse_mfg_data(out_result->mfg, out_result);
 }
 
 parser_status_t extract_mfg_data(const uint8_t* adv_data, size_t adv_data_len, mfg_data_t* out)
 {
   if (adv_data_len < MIN_ADV_DATA_LEN) { return PARSER_ERR_ADV_DATA_LEN; }
-
+	
   int i = 0;
 
   while (i < adv_data_len)
@@ -38,7 +39,13 @@ parser_status_t extract_mfg_data(const uint8_t* adv_data, size_t adv_data_len, m
 
       out->company_id = adv_data[i] | (adv_data[i+1] << 8);
       out->version_mode = adv_data[i+2];
-      out->flags = adv_data[i+3];      
+      out->flags = adv_data[i+3];
+			
+			if (field_len <= PROTOCOL_HEADER_SIZE) { return PARSER_ERR_MFG_DATA; }
+			
+			memcpy(out->payload, &adv_data[i+4], field_len-5);
+
+			out->payload_len = field_len-5;
 
       return PARSER_SUCCESS;
     }
@@ -49,43 +56,37 @@ parser_status_t extract_mfg_data(const uint8_t* adv_data, size_t adv_data_len, m
   return PARSER_ERR_MFG_DATA;
 }
 
-parser_status_t parse_mfg_data(mfg_data_t* mfg)
+parser_status_t parse_mfg_data(mfg_data_t* mfg, parser_result_t* out_result)
 {
   if (mfg->company_id != COMPANY_ID) { return PARSER_ERR_COMPANY_ID; }
 
-  return parse_protocol_msg((mfg->version_mode & 0x0F), mfg);
+  return parse_protocol_msg((mfg->version_mode & 0x0F), mfg, out_result);
 }
 
-parser_status_t parse_protocol_msg(parser_msg_t type, mfg_data_t* mfg)
+parser_status_t parse_protocol_msg(parser_msg_t type, mfg_data_t* mfg, parser_result_t* out_result)
 {
   switch(type)
   {
     case PARSER_PAIRING_MSG:
-      return handle_pairing_msg(mfg);
+			out_result->action = g_actions->on_pairing;
+			break;
     case PARSER_PAIRED_MSG:
-      return handle_paired_msg(mfg);
+			out_result->action = g_actions->on_paired;
+			break;
     case PARSER_LOST_MSG:
-      return handle_lost_msg(mfg);
+      out_result->action = g_actions->on_lost;
+			break;
     default:
       return PARSER_ERR_MODE;
 
   }
-
-  return PARSER_SUCCESS;
+	
+	out_result->mfg = mfg;
+	
+	return PARSER_SUCCESS;
 }
 
-parser_status_t handle_lost_msg(mfg_data_t* mfg)
+void parser_init(parser_action_table_t* actions)
 {
-  printf("\nHandle Lost Msg\n");
-  return PARSER_SUCCESS;
-}
-
-parser_status_t handle_pairing_msg(mfg_data_t* mfg)
-{
-  return PARSER_SUCCESS;
-}
-
-parser_status_t handle_paired_msg(mfg_data_t* mfg)
-{
-  return PARSER_SUCCESS;
+	g_actions = actions;
 }
