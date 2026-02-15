bdbc356da4a745820196a3161b7fde0f2106224e
Danielx2003 <ahipsterwalrus4@gnail.com>
2026-02-15 01:35:16 +0000

    refactor(dsic): reworked handlers to be implementation specific

--- main/src/disc_nimble.c
+++ main/src/disc_nimble.c
@@ -9,11 +9,94 @@
 
 static const char* tag = "BLE_DISC";
 
+void ble_handle_lost(void* context, mfg_data_t* mfg);
+void ble_handle_pairing(void* contexnt, mfg_data_t* mfg);
+void ble_handle_paired(void* context, mfg_data_t* mfg);
+
+static parser_action_table_t ble_actions = {
+    .on_pairing = ble_handle_pairing,
+    .on_paired  = ble_handle_paired,
+    .on_lost    = ble_handle_lost
+};
+
+static uint8_t own_addr_type;
+
+void ble_handle_lost(void* context, mfg_data_t* mfg)
+{
+  printf("\nHandle Lost Msg\n");
+	struct ble_gap_event* event = (struct ble_gap_event*)context;
+	printf("Event Type: %d\n", event->type);
+}
+
+void ble_handle_pairing(void* context, mfg_data_t* mfg)
+{
+	printf("\n\nHanlde Pairing Msg\n\n");
+	struct ble_gap_event* event = (struct ble_gap_event*)context;
+	
+//	struct ble_gap_conn_params params;
+//	memset(&params, 0, sizeof(params));
+//	
+	printf("Expected: %d. Actual: %d\n", event->ext_disc.length_data, mfg->payload_len);
+//	
+//	for (int i=0; i<event->ext_disc.length_data; i++)
+//	{
+//		printf("%02x:", event->ext_disc.data[i]);
+//	}
+//	
+//	printf("----\n\n");
+//	
+	for (int i=0; i<mfg->payload_len; i++)
+	{
+		printf("%02x:", mfg->payload[i]);
+	}
+	
+	printf("----\n\n");
+
+
+//	int rc= ble_gap_connect(
+//		own_addr_type, 
+//		&(event->ext_disc.addr),
+//		0,
+//		&params,
+//		NULL, NULL);
+//	
+//	printf("Tried to connect to device: Rc=%d", rc);
+}
+
+void ble_handle_paired(void* context, mfg_data_t* mfg)
+{
+	struct ble_gap_event* event = (struct ble_gap_event*)context;
+	printf("Event Type: %d\n", event->type);
+}
+
 int disc_cb(struct ble_gap_event *event, void *arg)
 {
 	parser_status_t status;
-	status = parse_adv_data(event->ext_disc.data, event->ext_disc.length_data);
+	mfg_data_t mfg;
+	memset(&mfg, 0, sizeof(mfg));
+	parser_result_t result;
+	
+	result.mfg = &mfg;
+	
+//	memset(&result, 0, sizeof(result));
+//	memset(result.mfg, 0, sizeof(*result.mfg));
+//	result.mfg->payload_len = 0;
+	
+	switch(event->type)
+	{
+		case BLE_GAP_EVENT_DISC_COMPLETE:
+			printf("Connection Complete?\n");
+			break;
+
+		default:
+			break;
+	}
+	
+	status = parse_adv_data(event->ext_disc.data, event->ext_disc.length_data, &result);
 	if (status != PARSER_SUCCESS) { return status; }
+	
+	result.action(event, result.mfg);
+	
 	return 0;
 }
 
@@ -23,7 +106,8 @@ ble_status_t disc_start(
 )
 {
   int rc;
-  uint8_t own_addr_type;
+	
+	parser_init(&ble_actions);
   
   rc = ble_hs_util_ensure_addr(0);
   if (rc != 0) { ESP_LOGE(tag, "No valid Address available. rc=%d\n", rc); return rc; }
