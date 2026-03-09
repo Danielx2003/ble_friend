eb2e2006102de33126a8e1a5026ca12fa0556e9d
Danielx2003 <ahipsterwalrus4@gnail.com>
2026-03-09 19:55:39 +0000

    feat: register ecdsa key + sign messages

--- main/include2/request/request2.h
+++ main/include2/request/request2.h
@@ -1,18 +1,37 @@
 #pragma once
 
-#include "../request/request_worker2.h"
+#include "request_worker2.h"
 #include "crypto2.h"
 
 typedef enum {
 	REQUEST_SUCCESS,
 	REQUEST_ERR_INIT,
-	REQUEST_ERR_NO_MEMORY
+	REQUEST_ERR_NO_MEMORY,
+	REQUEST_ERR_UNKNOWN
 } request_status_t;
 
 typedef struct {
-	crypto_key_t *finder_key;
-	uint8_t location; // Filler until we decide how we get location
+	char device_id[36];
+	uint8_t location[2]; // Filler until we decide how we get location
+	crypto_key_t *finder_key; // Public key that encrypted the report
+	uint8_t signature[64];
 } request_lost_payload_t;
+
+typedef struct {
+	char device_id[36];
+	uint8_t location[2];
+	uint8_t signature[64];
+	uint8_t public_key[32];
+//	size_t public_key_len;
+} request_lost_wire_t;
+
+typedef struct {
+	crypto_key_t *ecdsa_public_key;
+} request_ecdsa_payload_t;
+
+typedef struct {
+	char uuid[36];
+} request_ecdsa_response_t;
 
 /* Public API */
 
@@ -30,6 +49,10 @@ Owner requests all location's with the given public key
 Then he can decrypt - which can be done elsewhere(?)
 */
 request_status_t get_all_locations();
+
+request_status_t send_ecdsa_public_key(
+	request_ecdsa_payload_t *payload,
+	request_ecdsa_response_t *response);
 
 /*
 Start Request Task/Qeueue
--- main/src2/request2.c
+++ main/src2/request2.c
@@ -1,4 +1,5 @@
 #include "request2.h"
+#include "crypto2.h"
 #include "freertos/idf_additions.h"
 #include "request_worker2.h"
 #include "ble2.h"
@@ -54,28 +55,22 @@ request_status_t upload_lost_details(request_lost_payload_t *payload)
 	esp_http_client_handle_t client = esp_http_client_init(&config);
 
 	esp_http_client_set_method(client, HTTP_METHOD_POST);
-	esp_http_client_set_header(client, "Content-Type", "application/json");
+	esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
 	
-//	uint8_t data[128];
-//	char hex[65]; 
-//
-//	for (int i = 0; i < 32; i++) {
-//	    sprintf(&hex[i * 2], "%02X",
-//	            payload->finder_key->raw.data[i]);
-//	}
-//	hex[64] = '\0';
-//
-//	int len = snprintf((char *)data, sizeof(data),
-//	                   "{\"key\":\"%s\"}",
-//	                   hex);
-//
-//	if (len < 0 || len >= sizeof(data)) {
-//	    return -1;
-//	}
+	request_lost_wire_t wire_payload;
 
-	char data[] = "{\"title\": \"received lost msg!\"}";
+	memcpy(wire_payload.device_id, payload->device_id, 36);
+	memcpy(wire_payload.location, payload->location, 2);
+	memcpy(wire_payload.public_key, payload->finder_key->raw.data, 32);
+//	wire_payload.public_key_len = payload->finder_key->raw.len;
+	memcpy(wire_payload.signature, payload->signature, 64);
 
-	esp_http_client_set_post_field(client, data, sizeof(data)-1);
+	esp_http_client_set_post_field(
+		client,
+		(char *)&wire_payload,
+		sizeof(wire_payload)
+	);
+
 	esp_err_t err = esp_http_client_perform(client);
 	
 	if (err == ESP_OK) {
@@ -88,6 +83,67 @@ request_status_t upload_lost_details(request_lost_payload_t *payload)
 	esp_http_client_cleanup(client);
 
   return REQUEST_SUCCESS;
+}
+
+
+esp_err_t send_ecdsa_public_key_event_handler(esp_http_client_event_t *evt)
+{
+	request_ecdsa_response_t *response = (request_ecdsa_response_t *)evt->user_data;
+
+  switch(evt->event_id) {
+    case HTTP_EVENT_ON_DATA:
+      ESP_LOGI(TAG, "Received %d bytes", evt->data_len);
+      printf("%.*s\n", evt->data_len, (char*)evt->data);
+			memcpy(response->uuid, evt->data, 36);
+      break;
+
+    default:
+      break;
+  }
+
+  return ESP_OK;
+}
+
+
+request_status_t send_ecdsa_public_key(request_ecdsa_payload_t *payload, request_ecdsa_response_t *response)
+{
+	esp_http_client_config_t config = {
+		.url = "http://192.168.0.172:3000/register",
+		.event_handler = send_ecdsa_public_key_event_handler,
+		.user_data = response
+	};
+	esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	esp_http_client_set_method(client, HTTP_METHOD_POST);
+
+	crypto_key_t public_key;
+	crypto_status_t status = export_ecdsa_public_key(
+		payload->ecdsa_public_key,
+		&public_key
+	);
+	if (status != CRYPTO_SUCCESS) { return REQUEST_ERR_UNKNOWN; }
+
+	esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
+
+	esp_http_client_set_post_field(
+	    client,
+	    (char*)public_key.raw.data,
+	    public_key.raw.len
+	);
+
+	esp_err_t err = esp_http_client_perform(client);
+
+	if (err == ESP_OK) {
+	    ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRId64,
+	            esp_http_client_get_status_code(client),
+	            esp_http_client_get_content_length(client));
+	} else {
+	    ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+	}
+
+	esp_http_client_cleanup(client);
+
+	return REQUEST_SUCCESS;
 }
 
 request_status_t request_init()
@@ -109,6 +165,6 @@ request_status_t request_init()
 		NULL,
 		1
 	);
-			
+
 	return REQUEST_SUCCESS;
 }
