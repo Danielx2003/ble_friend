706fbbc395ccbc770464e8d9fe14d6ac3d3f12a7
Danielx2003 <ahipsterwalrus4@gnail.com>
2026-03-30 15:31:48 +0100

    feat(request): upload lost location report

--- main/include2/request/request2.h
+++ main/include2/request/request2.h
@@ -1,40 +1,47 @@
 #pragma once
 
-#include "../request/request_worker2.h"
+#include "request_worker2.h"
 #include "crypto2.h"
+
+#define MAX_BATCH_ITEMS 20
 
 typedef enum {
 	REQUEST_SUCCESS,
 	REQUEST_ERR_INIT,
-	REQUEST_ERR_NO_MEMORY
+	REQUEST_ERR_NO_MEMORY,
+	REQUEST_ERR_UNKNOWN
 } request_status_t;
 
 typedef struct {
-	crypto_key_t *finder_key;
-	uint8_t location; // Filler until we decide how we get location
-} request_lost_payload_t;
+	size_t size;
+	request_lost_payload_t payloads[MAX_BATCH_ITEMS];
+} request_lost_batch_wire_t;
 
 typedef struct {
-	uint8_t bssid[6];
-	int8_t rssi;
-} my_wifi_ap_record_t;
+	char device_id[36];
+	uint8_t location[2];
+	uint8_t signature[64];
+	uint8_t public_key[32];
+//	size_t public_key_len;
+} request_lost_wire_t;
 
 typedef struct {
-	uint8_t number_aps;
-	my_wifi_ap_record_t aps[3];
-} request_device_location_payload_t;
+	crypto_key_t *ecdsa_public_key;
+} request_ecdsa_payload_t;
+
+typedef struct {
+	char uuid[36];
+} request_ecdsa_response_t;
 
 /* Public API */
 
-request_status_t upload_batch(request_work_item_t *batch, size_t batch_len);
+request_status_t upload_lost_batch(request_lost_payload_t *batch, size_t batch_len);
 
 /*
 POST
 Upload finders public key, encrypted lost message
 */
 request_status_t upload_lost_details(request_lost_payload_t *payload);
-
-request_status_t get_device_location_from_bssid(request_device_location_payload_t *payload);
 
 /*
 GET
@@ -43,6 +50,9 @@ Then he can decrypt - which can be done elsewhere(?)
 */
 request_status_t get_all_locations();
 
+request_status_t send_ecdsa_public_key(
+	request_ecdsa_payload_t *payload,
+	request_ecdsa_response_t *response);
 
 /*
 Start Request Task/Qeueue
--- main/src2/request2.c
+++ main/src2/request2.c
@@ -14,10 +14,10 @@ QueueHandle_t request_worker_queue = NULL;
 
 /* Request Functions */
 
-request_status_t upload_batch(request_work_item_t *batch, size_t batch_len)
+
+request_status_t upload_lost_batch(request_lost_payload_t *batch, size_t batch_len)
 {
 	payloads_received += batch_len;
-//	printf("uploaded %d reports\n", batch_len);
 
 //	esp_http_client_config_t config = {
 //		.url = "http://192.168.0.172:3000/send",
@@ -25,11 +25,26 @@ request_status_t upload_batch(request_work_item_t *batch, size_t batch_len)
 //	esp_http_client_handle_t client = esp_http_client_init(&config);
 //
 //	esp_http_client_set_method(client, HTTP_METHOD_POST);
-//	esp_http_client_set_header(client, "Content-Type", "application/json");
+//	esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
 //	
-//	char data[] = "{\"title\": \"batch upload!\"}";
+//	request_lost_batch_wire_t data = {
+//		.size = batch_len,
+//	};
 //
-//	esp_http_client_set_post_field(client, data, sizeof(data)-1);
+//	memcpy(data.payloads, batch, sizeof(request_lost_payload_t) * batch_len);
+//	
+//	esp_http_client_set_post_field(
+//		client,
+//		(char *)&data,
+//		sizeof(size_t) + sizeof(request_lost_payload_t) * batch_len
+//	);
+//	
+//	printf("Data size: %d - Payload size: %d - Num Payloads: %d\n",
+//		sizeof(data),
+//		sizeof(size_t) + sizeof(request_lost_payload_t) * batch_len,
+//		batch_len
+//	);
+//
 //	esp_err_t err = esp_http_client_perform(client);
 //
 //	if (err == ESP_OK) {
@@ -39,119 +54,29 @@ request_status_t upload_batch(request_work_item_t *batch, size_t batch_len)
 //	} else {
 //	    ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
 //	}
-//
 //	esp_http_client_cleanup(client);
-//	esp_http_client_close(client);
-	
+
 	return REQUEST_SUCCESS;
 }
 
 request_status_t upload_lost_details(request_lost_payload_t *payload)
 {
-	esp_http_client_config_t config = {
-		.url = "http://192.168.0.172:3000/send",
-	};
-	esp_http_client_handle_t client = esp_http_client_init(&config);
-
-	esp_http_client_set_method(client, HTTP_METHOD_POST);
-	esp_http_client_set_header(client, "Content-Type", "application/json");
-	
-//	uint8_t data[128];
-//	char hex[65]; 
+//	esp_http_client_config_t config = {
+//		.url = "http://192.168.0.172:3000/send",
+//	};
+//	esp_http_client_handle_t client = esp_http_client_init(&config);
 //
-//	for (int i = 0; i < 32; i++) {
-//	    sprintf(&hex[i * 2], "%02X",
-//	            payload->finder_key->raw.data[i]);
-//	}
-//	hex[64] = '\0';
-//
-//	int len = snprintf((char *)data, sizeof(data),
-//	                   "{\"key\":\"%s\"}",
-//	                   hex);
-//
-//	if (len < 0 || len >= sizeof(data)) {
-//	    return -1;
-//	}
-
-	char data[] = "{\"title\": \"received lost msg!\"}";
-
-	esp_http_client_set_post_field(client, data, sizeof(data)-1);
-	esp_err_t err = esp_http_client_perform(client);
-	
-	if (err == ESP_OK) {
-	    ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRId64,
-	            esp_http_client_get_status_code(client),
-	            esp_http_client_get_content_length(client));
-	} else {
-	    ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-	}
-	esp_http_client_cleanup(client);
-
-  return REQUEST_SUCCESS;
-}
-
-
-request_status_t get_device_location_from_bssid(request_device_location_payload_t *payload)
-{
-	esp_http_client_config_t config = {
-		.url = "http://192.168.0.172:3000/location",
-//	.url = "http://10.207.218.103:3000/location"
-	};
-	esp_http_client_handle_t client = esp_http_client_init(&config);
-
-	esp_http_client_set_method(client, HTTP_METHOD_GET);
-	esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
-	
-	my_wifi_ap_record_t record = {
-		.bssid = {0x64, 0xFA, 0x2B, 0x3A, 0x88, 0xD2},
-		.rssi = -66
-	};
-	
-	my_wifi_ap_record_t record2 = {
-		.bssid = {0x8C, 0x9A, 0x8F, 0x08, 0x9B, 0x3E},
-		.rssi = -67
-	};
-	
-	my_wifi_ap_record_t record3 = {
-		.bssid = {0x3C, 0x6A, 0xD2, 0xE9, 0x54, 0xD2},
-		.rssi = -78
-	};
-	
-	request_device_location_payload_t wire = {
-		.number_aps = 3,
-	};
-
-	memcpy(&wire.aps[0], &record, sizeof(record));
-	memcpy(&wire.aps[1], &record2, sizeof(record));
-	memcpy(&wire.aps[2], &record3, sizeof(record));
-	
-	esp_http_client_set_post_field(
-		client,
-		(char *)&wire,
-		sizeof(uint8_t) +  sizeof(my_wifi_ap_record_t)*3
-	);
-
-	esp_err_t err = esp_http_client_perform(client);
-	
-	if (err == ESP_OK) {
-	    ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRId64,
-	            esp_http_client_get_status_code(client),
-	            esp_http_client_get_content_length(client));
-	} else {
-	    ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-	}
-	esp_http_client_cleanup(client);
-	
-	return REQUEST_SUCCESS;
-	
+//	esp_http_client_set_method(client, HTTP_METHOD_POST);
+//	esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
+//	
 //	request_lost_wire_t wire_payload;
-
+//
 //	memcpy(wire_payload.device_id, payload->device_id, 36);
 //	memcpy(wire_payload.location, payload->location, 2);
 //	memcpy(wire_payload.public_key, payload->finder_key->raw.data, 32);
-//	wire_payload.public_key_len = payload->finder_key->raw.len;
+////	wire_payload.public_key_len = payload->finder_key->raw.len;
 //	memcpy(wire_payload.signature, payload->signature, 64);
-
+//
 //	esp_http_client_set_post_field(
 //		client,
 //		(char *)&wire_payload,
@@ -168,6 +93,64 @@ request_status_t get_device_location_from_bssid(request_device_location_payload_
 //	    ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
 //	}
 //	esp_http_client_cleanup(client);
+
+  return REQUEST_SUCCESS;
+}
+
+
+request_status_t get_device_location_from_bssid(request_device_location_payload_t *payload)
+{
+	esp_http_client_config_t config = {
+		.url = "http://192.168.0.172:3000/location",
+//	.url = "http://10.207.218.103:3000/location"
+	};
+
+	esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	esp_http_client_set_method(client, HTTP_METHOD_GET);
+	esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
+
+	my_wifi_ap_record_t record = {
+		.bssid = {0x64, 0xFA, 0x2B, 0x3A, 0x88, 0xD2},
+		.rssi = -66
+	};
+
+	my_wifi_ap_record_t record2 = {
+		.bssid = {0x8C, 0x9A, 0x8F, 0x08, 0x9B, 0x3E},
+		.rssi = -67
+	};
+
+	my_wifi_ap_record_t record3 = {
+		.bssid = {0x3C, 0x6A, 0xD2, 0xE9, 0x54, 0xD2},
+		.rssi = -78
+	};
+
+	request_device_location_payload_t wire = {
+		.number_aps = 3,
+	};
+
+	memcpy(&wire.aps[0], &record, sizeof(record));
+	memcpy(&wire.aps[1], &record2, sizeof(record));
+	memcpy(&wire.aps[2], &record3, sizeof(record));
+
+	esp_http_client_set_post_field(
+		client,
+		(char *)&wire,
+		sizeof(uint8_t) +  sizeof(my_wifi_ap_record_t)*3
+	);
+
+	esp_err_t err = esp_http_client_perform(client);
+
+	if (err == ESP_OK) {
+	    ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRId64,
+	            esp_http_client_get_status_code(client),
+	            esp_http_client_get_content_length(client));
+	} else {
+	    ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+	}
+	esp_http_client_cleanup(client);
+
+	return REQUEST_SUCCESS;
 }
 
 request_status_t request_init()
