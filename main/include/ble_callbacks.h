cbf0816e293178468580f20bd8e67203b53b210c
Danielx2003 <ahipsterwalrus4@gnail.com>
2026-04-17 16:19:16 +0100

    refactor: deleted unused files, file cleanup

--- main/include/ble_callbacks.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "parser.h"
-
-bool on_pairing_msg(void *ctx, mfg_data_t *mfg);
-bool on_lost_msg(void *ctx, mfg_data_t *mfg);
-bool on_paired_msg(void *ctx, mfg_data_t *mfg);
